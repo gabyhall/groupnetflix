20e91c77744bcef687f42c2bfe3dbfe6928c51ea
abdi hassan <abdifatahyh@gmail.com>
2021-08-04 17:28:34 +0100

    abdi's update

--- src/utils/index.js
+++ src/utils/index.js
@@ -22,3 +22,46 @@ export const fetchUsers = async (e, email, username, pass, setUser, user) => {
         console.log(error)
     }
 }
+
+export const updateUser = async (e, email, username, pass, setUser, user) => {
+    e.preventDefault();
+    try {
+        let response;
+        if (username) {
+            response = await fetch(`${process.env.REACT_APP_REST_API}users`, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    email: email,
+                    username: username,
+                    password: pass,
+                    currentUser: user
+                })
+            })
+    }
+    const data = await response.json();
+    setUser(data.user.username)
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+export const deleteUser = async (user, setUser) => {
+    try {
+        let response;
+        if (user) {
+            response = await fetch(`${process.env.REACT_APP_REST_API}users/${user}`, {
+                method: 'DELETE',
+                headers: {'Content-Type': 'application/json'},
+                username: user,
+            })
+    }
+    await response.json();
+    setUser()
+    } catch (error) {
+        console.log(error);
+    }
+};
+   
+
