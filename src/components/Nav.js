1044dcd7db035665ca66e524f7c52f22e1752b45
patto1878 <1878patto@gmail.com>
2021-08-04 17:22:20 +0100

    patto first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Nav.css"
+import logo from '../images/nflogo.png'
+
+function Nav() {
+    return ( <div className="Nav"><img src={logo} alt="nflogo" />
+<div className="login">  
+        <button>Log in</button> 
+        <button>Sign Up </button> Movies Watch List
+        
+        
+        
+        </div> 
+
+    </div>
+        
+
+    )
+};
+
+export default Nav
