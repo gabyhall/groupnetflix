ffa7b6f70346db78d5a07feae8e6a98c5b7e62eb
Gaby Hall <gabyhall@Jonathan-Robertss-iMac.local>
2021-08-05 16:15:01 +0100

    add movies?

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+
+
 export const fetchUsers = async (e, email, username, pass, setUser, user) => {
     e.preventDefault();
     try {
@@ -15,10 +17,33 @@ export const fetchUsers = async (e, email, username, pass, setUser, user) => {
     } else {
         response = await fetch(`${process.env.REACT_APP_REST_API}users/${username}`, )
     } 
-    const data = await response.json();
-    console.log(data); 
-    setUser(data.user.username) //might be different depending on format of my database //
+    const data = await response.json(); 
+    setUser(data.user.username)
     } catch (error) {
         console.log(error)
     }
+}
+
+
+
+export const addToList = async (item, setAddFilm) => {
+        try {
+            let response;
+            response = await fetch(`${process.env.REACT_APP_REST_API}movies`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    user: "username",           // showing up as null in db//
+                    title: item.title,          // saving to db//
+                    release: item.release_date, // not saving to db//
+                    imageURL: item.poster_path, // not saving to db//
+                    filmID: item.id,            // not saving to db//
+                }) 
+            })
+            const data = await response.json();
+            console.log(data);
+            setAddFilm(data);
+        } catch (error) {
+            console.log(error)
+        }
 }
