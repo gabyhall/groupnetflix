23323a7cc4122c8848721ae33f492d07973d92b7
patto1878 <1878patto@gmail.com>
2021-08-05 15:31:02 +0100

    2nd com

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,53 @@
 import React from 'react'
 import "./Nav.css"
 import logo from '../images/nflogo.png'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
 
 function Nav() {
     return ( <div className="Nav"><img src={logo} alt="nflogo" />
 <div className="login">  
         <button>Log in</button> 
         <button>Sign Up </button> Movies Watch List
+        <ul class="navbar-right">
+                <li class="menuItem">
+                <Link class="menuItem" to="/" >Home</Link>
+                </li>
+                <li class="menuItem">
+                <Link class="menuItem" to="/Movies" >Movies</Link>
+                </li>
+                <li class="menuItem">
+                <Link class="menuItem" to="/MyWatchList">My Watch List</Link>
+                </li>
+                <li class="menuItem">
+                <Link class="menuItem" to="/Profile">Profile</Link>
+                </li>
+              </ul>
+
+
+        <Switch>
+  <Route path="/Movies">
+    <div className="moviesPage">
+    </div>
+  </Route>
+  <Route path="/MyWatchList">
+    <div className="myWatchListPage">
+    </div>
+  </Route>
+  <Route path="/Profile">
+    <div className = "profilePage">
+    </div>
+  </Route>
+  <Route path="/">
+    <div className = "homePage">
+    </div>
+  </Route>
+</Switch>
         
         
         
