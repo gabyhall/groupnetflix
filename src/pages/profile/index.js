ea4f5f76df515d614e2d9d034e6a377dc2ac8f73
danielf-11 <danielfong8@gmail.com>
2021-08-06 11:32:55 +0100

    CSS and React Router

---
+++ src/pages/profile/index.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { updateUser } from "../../utils";
+import { deleteUser } from "../../utils";
+import { PageContainer } from '../../styledComponents';
+import { Nav } from '../../components/navbar'
+
+
+const Profile = ({user, setUser}) => {
+    const [email, setEmail] = useState();
+    const [username, setUsername] = useState();
+    const [pass,setPassword] = useState();
+
+    return (
+        <PageContainer>
+            <div className ="profileContainer">
+            <h1>User Details</h1>
+                <form onSubmit={(e) => updateUser(e, email, username, pass, setUser, user)}>
+                    <label>
+                        <p>Change username:</p>
+                        <input type="text" name="username" onChange = {(e) => setUsername(e.target.value)} />
+                    </label>
+                    <label>
+                        <p>Change email:</p>
+                        <input type="text" name="email" onChange = {(e) => setEmail(e.target.value)} />
+                    </label>
+                    <label>
+                        <p>Change password:</p>
+                        <input type="text" name="password" onChange = {(e) => setPassword(e.target.value)} />
+                    </label>
+                        <button type="submit" >Update</button>
+                </form>
+
+                <p>Delete User Profile</p>
+                <button type="submit" onClick = {() => {deleteUser(setUser, user)}}>Delete</button>
+                </div>
+        </PageContainer>
+    )
+};
+
+export default Profile;
